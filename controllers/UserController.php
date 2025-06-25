a2e16d4ff01494df85ef7218341d090de7fb13aa
Talbi Said <eyestech404@protonmail.com>
2025-06-25 14:31:45 +0000

    Étape 9

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -12,8 +12,27 @@ class UserController {
     }
 
     public function checkCreate() {
-        
+        // 1. Récupérer les données du formulaire (POST)
+        $email = $_POST['email'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+    
+        // 1.2. Hydrater une instance de User
+        require_once 'models/User.php';
+        $user = new User($email, $first_name, $last_name);
+    
+        // 2. Instancier le manager et transmettre l'utilisateur
+        require_once 'managers/UserManager.php';
+        $manager = new UserManager();
+        $manager->create($user);
+    
+        // 3. Rediriger vers la liste des utilisateurs
+        header('Location: index.php?route=list');
+        exit;
     }
+
+
+
     
     public function update() {
         $route = "update";
